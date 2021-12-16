788442c897ad4bf91b8002c10ac541a12e046ce7
qhalbym <qhalbym@gmail.com>
2021-12-16 15:15:13 +0700

    Awal

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ function randomQuestion(array) {
   //random pertanyaan2
 }
 
-// array of object pertanyaan
 let pertanyaan = [{
-    isiPertanyaan: "",
-    jawabanBenar:"",
+    question: "",
+    option: ["","","",""],
     jawabanSalah:["","",""]
   }
 ]
